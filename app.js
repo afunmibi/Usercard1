bbb4d3114aa46dbc203ba44ff382018dc1f35eb0
afunmibi <112468468+afunmibi@users.noreply.github.com>
2023-06-20 12:58:31 +0100

    'usercard'

---
+++ app.js
@@ -0,0 +1,9 @@
+
+// work inprogress
+// .addEventListener('click', () => {
+// 	social_panel_container.classList.toggle('visible')
+// });
+
+// .addEventListener('click', () => {
+// 	social_panel_container.classList.remove('visible')
+// });
